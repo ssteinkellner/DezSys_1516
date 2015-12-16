bd3b0800e10fee6cc0f2291bef145080a8bdc7ce
SSteinkellner <ssteinkellner@student.tgm.ac.at>
2015-12-16 22:51:43 +0100

    sql-where fuer suche begonnen

--- src/main/java/ss_ts/Eintrag.java
+++ src/main/java/ss_ts/Eintrag.java
@@ -31,4 +31,38 @@ public class Eintrag {
 	public static Eintrag load(String titel){
 		return new Eintrag(titel,new String[0],"");
 	}
+	
+	/**
+	 * 
+	 * @param suchbegriffe ein String array mit suchbegriffen. je früher ein eintrag kommt, desto weiter wird er eingekapselt.
+	 * begriffe die mit "& " beginnen, werden mit und, alles andere mit oder zusammengehängt
+	 * @return
+	 */
+	public static Eintrag[] search(String[] suchbegriffe){
+		String where = "";
+		boolean lastIsAnd = false;
+		int sub;
+		
+		for(String begriff : suchbegriffe){
+			sub = 0;
+			if(!where.isEmpty()){
+				if(begriff.startsWith("& ")){
+					sub = 2;
+					if(!lastIsAnd) where = " (" + where + ") ";
+					where += " AND ";
+					lastIsAnd = true;
+				}else{
+					if(lastIsAnd) where = " (" + where + ") ";
+					where += " OR ";
+					lastIsAnd = false;
+				}
+			}
+			
+			where += begriff.substring(sub);
+		}
+		
+		System.out.println(where);
+		
+		return new Eintrag[0];
+	}
 }
---
+++ test/main/java/test/Test_search.java
@@ -0,0 +1,11 @@
+package test;
+
+import ss_ts.Eintrag;
+
+public class Test_search {
+
+	public static void main(String[] args) {
+		Eintrag[] e = Eintrag.search(new String[]{"",""});
+	}
+
+}
