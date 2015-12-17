2f324e875b8aaef2ac27f54c756790f5e24d997d
SSteinkellner <ssteinkellner@student.tgm.ac.at>
2015-12-17 23:34:46 +0100

    connectionhandler aus aelterem projekt kopiert

---
+++ src/main/java/ss/connection/ConnectionHandler.java
@@ -0,0 +1,78 @@
+package ss.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ * eine klasse, die fuer das auf/abbauen von verbindungen zustaendig ist.
+ * es gibt maximal eine connection.
+ * @author SSteinkellner
+ * @version 2014.12.30
+ */
+public class ConnectionHandler {
+	private Connection connection;
+	private UserCache usercache;
+	private String host, database;
+	
+	public ConnectionHandler(UserCache userCache){
+		usercache = userCache;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+
+	public String getDatabase() {
+		return database;
+	}
+	
+	public Connection getConnection() {
+		if(connection==null){
+			connStart();
+		}
+		
+		return connection;
+	}
+	
+
+	/**
+	 * loads the database driver and connects to the database. closes the programm, if there is an error
+	 */
+	private void connStart() {
+
+		try {
+			Class.forName("org.postgresql.Driver").newInstance();
+		} catch(Exception ex) {
+			System.err.println("Can't find Database driver class!");
+			System.exit(1);
+		}
+
+		try {
+			connection = (Connection) DriverManager.getConnection("jdbc:mysql://" + host + "/" + database, usercache.getUser(), usercache.getPassword());
+			System.out.println("Successfully connected to " + database + " on " + host);
+		} catch(SQLException ex) {
+			System.err.println("DB connection error!");
+		}
+	}
+	
+	public void close(){
+		if(connection==null){
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				System.err.println("Couldn't close Connection!");
+			}
+		}
+	}
+
+}
