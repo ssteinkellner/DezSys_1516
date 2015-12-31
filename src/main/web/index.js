fda529c04bec584b12a28334b68f6ed41d9372e4
SSteinkellner <ssteinkellner@student.tgm.ac.at>
2015-12-31 16:27:12 +0100

    javascript hinzugefuegt

---
+++ src/main/web/index.js
@@ -0,0 +1,88 @@
+/**
+ * diese funktion wird mit einer tabellenzeile gefüttert und gibt die eintrags-eigenschaften als assoziatives array zurück
+ */
+function convertRow(row){
+	var elements = row.children;
+	
+	var entry = {
+		'name': elements[1].innerHTML,
+		'tags': elements[2].innerHTML,
+		'file': elements[3].children[0].innerHTML
+	};
+	
+	return entry;
+}
+
+/**
+ * diese funktion wird mit dem aufrufenden element gefüttert.
+ * sie füllt das html form zum löschen aus, macht dieses sichtbar und fokusiert darauf
+ * example: onclick="del(this)"
+ */
+function del(element){
+	closeDialogsExept('delete');
+	
+	var entry = convertRow(element.parentNode.parentNode);
+	var dialog = document.forms.delete;
+	
+	dialog.querySelector('span.fileName').innerHTML = entry.name;
+	dialog.name.value = entry.name;
+	
+	dialog.className = '';
+	dialog.querySelector('input[type=button]').focus();
+}
+
+/**
+ * diese funktion wird mit dem aufrufenden element gefüttert.
+ * sie füllt das html form zum ändern aus, setzt den titel, macht es sichtbar und fokusiert darauf
+ * example: onclick="edit(this)"
+ */
+function edit(element){
+	closeDialogsExept('put');
+	
+	var entry = convertRow(element.parentNode.parentNode);
+	var dialog = document.forms.put;
+	
+	dialog.querySelector('h2').innerHTML = 'Eintrag bearbeiten';
+	dialog.name.value = entry.name;
+	dialog.tags.value = entry.tags;
+	dialog.file.value = entry.file;
+	dialog.mode = 'update';
+	
+	dialog.className = '';
+	dialog.name.focus();
+}
+
+/**
+ * diese funktiopn setzt den titel des html form zum ändern, macht es sichtbar und fokusiert darauf
+ */
+function create(){
+	closeDialogsExept('put');
+	
+	var dialog = document.forms.put;
+	
+	dialog.querySelector('h2').innerHTML = 'Eintrag anlegen';
+	dialog.name.value = '';
+	dialog.tags.value = '';
+	dialog.file.value = '';
+	dialog.mode = 'insert';
+	
+	dialog.className = '';
+	dialog.name.focus();
+}
+
+/**
+ * macht alle dialoge ausser dem angegebenen unsichtbar
+ */
+function closeDialogsExept(thisOne){
+	var dialogs = ['put','delete'];
+	
+	for(dialog in dialogs) {
+		if(dialog != thisOne){
+			document.forms[dialog].className='hidden';
+		}
+	}
+}
+
+function closeDialogs(){
+	closeDialogsExept(null);
+}
--- src/main/web/mainForm.php
+++ src/main/web/mainForm.php
@@ -2,30 +2,37 @@
 <html>
 	<head>
 		<title>ReSTful</title>
-		<link rel="Stylesheet" type="text/css" href="index.css" >
+		<link rel="Stylesheet" type="text/css" href="index.css">
+		<script type="text/javascript" src="index.js"></script>
 	</head>
 	<body>
 		<h1>ReSTful</h1>
 		
-		<font style="color:#FF0000; display:block;">
+		<span style="color:#FF0000; display:block;">
 			<?php if(isset($err) && !empty($err)) echo $err; ?>
-		</font>
-		<font style="color:#00AAAA; display:block;">
+		</span>
+		<span style="color:#00AAAA; display:block;">
 			<?php if(isset($msg) && !empty($msg)) echo $msg; ?>
-		</font>
-		<font style="color:#00AA00; display:block;">
+		</span>
+		<span style="color:#00AA00; display:block;">
 			<?php if(isset($dbg) && !empty($dbg)) echo $dbg; ?>
-		</font>
+		</span>
 		
 		<h2>vorhandene Eintr&auml;ge</h2>
 		
+		<span onClick="create();" class="bt">Eintrag anlegen</span>
 		<table border=1>
 			<tr>
+				<th>Aktionen</th>
 				<th>Name</th>
 				<th>Suchbegriffe</th>
 				<th>Datei</th>
 			</tr>
 			<tr>
+				<td>
+					<span onClick="edit(this);" class="bt">bearbeiten</span>,
+					<span onClick="del(this);" class="bt">l&ouml;schen</span>
+				</td>
 				<td>Beispiel</td>
 				<td>beispiel, bsp</td>
 				<td>
@@ -34,6 +41,10 @@
 			</tr>
 			<?php if(isset($eintraege)) foreach($eintraege as $eintrag){ ?>
 			<tr>
+				<td>
+					<span onClick="edit(this);" class="bt">bearbeiten</span>,
+					<span onClick="del(this);" class="bt">l&ouml;schen</span>
+				</td>
 				<td><?php echo $eintrag['name']; ?></td>
 				<td><?php echo $eintrag['tags']; ?></td>
 				<td>
@@ -43,49 +54,30 @@
 			<?php } ?>
 		</table>
 		
-		
-		<h2>Eintrag anlegen</h2>
-		
-		<form method="PUT">
+		<form name="put" class="hidden" method="POST">
+			<h2><!-- Eintrag anlegen | Eintrag bearbeiten --></h2>
+			
 			<input type="text" name="name" placeHolder="Name">
 			<input type="text" name="tags" placeHolder="Suchbegriffe">
 			<input type="text" name="file" placeHolder="Datei">
 			
-			<input type="submit" value="speichern">
+			<input type="hidden" name="mode" value="<!-- insert | update | delete -->">
+			<input type="submit" value="Speichern">
+			<input type="button" onclick="closeDialogs();" value="Abbrechen">
 		</form>
 		
+		<form name="delete" class="hidden" method="POST">
+			<h2>Eintrag l&ouml;schen</h2>
 		
-		<h2>Eintrag bearbeiten</h2>
-		
-		<form method="PUT">
-			<select name="name">
-				<option disabled selected label="bitte w&auml;hlen">
-				<option label="Beispiel" value="Beispiel">
-				<?php if(isset($eintraege)) foreach($eintraege as $eintrag){ ?>
-				<option label="<?php echo $eintrag['name']; ?>" value="<?php echo $eintrag['name']; ?>">
-				<?php } ?>
-			</select>
-			<input type="text" name="tags" placeHolder="Suchbegriffe">
-			<input type="text" name="file" placeHolder="Datei">
+			<input type="hidden" name="name">
+			<p>
+				Sind Sie sicher, dass Sie <span class="fileName"></span> l&ouml;schen wollen?
+			</p>
 			
-			<input type="submit" value="speichern">
+			<input type="hidden" name="mode" value="delete">
+			<input type="submit" value="Fortfahren">
+			<input type="button" onclick="closeDialogs();" value="Abbrechen">
 		</form>
-		
-		
-		<h2>Eintrag l&ouml;schen</h2>
-		
-		<form method="DELETE">
-			<select name="name">
-				<option disabled selected label="bitte w&auml;hlen">
-				<option label="Beispiel" value="Beispiel">
-				<?php if(isset($eintraege)) foreach($eintraege as $eintrag){ ?>
-				<option label="<?php echo $eintrag['name']; ?>" value="<?php echo $eintrag['name']; ?>">
-				<?php } ?>
-			</select>
-			<input type="submit" value="l&ouml;schen">
-		</form>
-		
-		
 		
 		<div style="position:fixed; bottom:0; right:0; padding:5px;">
 			created by Sebastian Steinkellner
