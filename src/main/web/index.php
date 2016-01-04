7ac4b08a7644578e177ee6a166151efdc002efc0
SSteinkellner <ssteinkellner@student.tgm.ac.at>
2016-01-04 12:13:10 +0100

    php versionscheck angepasst

--- src/main/web/index.php
+++ src/main/web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-if (version_compare(/*PHP_VERSION*/'5.3', '5.4') < 0) die('<h3 style="color:red;">PHP 5.4 or higher needed!</h3>');
+if (version_compare(PHP_VERSION, '5.4') < 0) die('<h3 style="color:red;">PHP 5.4 or higher needed!</h3>');
 
 $err = '';
 $msg = '';
