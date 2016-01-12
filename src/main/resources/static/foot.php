4623471fc65d85afbab3e5a45c20f3d7e433212a
SSteinkellner <ssteinkellner@student.tgm.ac.at>
2016-01-12 15:18:25 +0100

    umarbeiten php zu thymeleaf

--- src/main/resources/static/foot.php
+++
@@ -1,3 +0,0 @@
-<div style="position:fixed; bottom:0; right:0; padding:5px;">
-	created by Sebastian Steinkellner
-</div>
---
+++ target/classes/static/files/wikipedia.php
@@ -0,0 +1,1 @@
+<?php header('Location: https://de.wikipedia.org/wiki/Wikipedia'); ?>
