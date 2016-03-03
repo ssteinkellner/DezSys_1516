74a06017dcffc17484d0b2c00e48e16f9d56210c
Steinkellner Sebastian <ssteinkellner@student.tgm.ac.at>
2016-03-04 00:00:43 +0100

    Debug Info extended

--- src/ak_ss/service/PiService.java
+++ src/ak_ss/service/PiService.java
@@ -17,8 +17,9 @@ public class PiService implements Service {
     	}
 
         double pi = 0, dotx, doty;
-        int innerhalb = 0;
+        int innerhalb = 0, log = 10000000;
 
+        if(tropfenanzahl >= log) System.out.print("[");
         for(int gesamt = tropfenanzahl; gesamt > 0; gesamt--){ // generiere Tropfen und addiere je nach Zugehörigkeit
             dotx = Math.random();
             doty = Math.random();
@@ -26,8 +27,10 @@ public class PiService implements Service {
             if (dotx*dotx + doty*doty <= 1) innerhalb++; // Punkt liegt innerhalb des Kreises
             // else Punkt liegt außerhalb des Kreises
 
+            if(gesamt % 10000000 == 0) System.out.print("#");
  //           if(gesamt % 10000000 == 0) System.out.print("Nur noch " + gesamt + " Durchlaeufe!\r");
         }
+        if(tropfenanzahl >= log) System.out.println("]");
 
         pi = 4*(double)innerhalb/tropfenanzahl;
         return ""+pi;
