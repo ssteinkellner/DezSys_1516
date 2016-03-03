0041354c6037785d3917e8e0c0c47d34074e3d18
Alexander Kölbl <alexander.koelbl@student.tgm.ac.at>
2016-03-03 18:37:31 +0100

    LeastConnection Algorithm

--- src/ak_ss/balancer/algorithms/LeastConnection.java
+++ src/ak_ss/balancer/algorithms/LeastConnection.java
@@ -1,5 +1,7 @@
 package ak_ss.balancer.algorithms;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import ak_ss.balancer.Algorithm;
@@ -13,15 +15,27 @@ import ak_ss.balancer.Node;
  */
 public class LeastConnection implements Algorithm {
 
+	private Collection<Node> nodes;
 	
 	public LeastConnection(List nodes){
-		
+		this.nodes = nodes;
 	}
 	
 	@Override
 	public Node getNext() {
-		
-		return null;
+		Iterator<Node> i = nodes.iterator();
+		int min = -1;
+		Node least = null;
+
+		while(i.hasNext()) {
+			Node n = i.next();
+			int count = n.getTasks().size();
+			if(count < min || min == -1){
+				min = count;
+				least = n;
+			}
+		}
+		return least;
 	}
 	
 }
