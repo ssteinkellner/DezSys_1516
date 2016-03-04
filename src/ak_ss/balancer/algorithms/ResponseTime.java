71e446cc789f7551507b3b5031258fabdca6e53a
Alexander Kölbl <alexander.koelbl@student.tgm.ac.at>
2016-03-04 12:02:39 +0100

    added ResponseTime Algorithm

--- src/ak_ss/balancer/algorithms/ResponseTime.java
+++ src/ak_ss/balancer/algorithms/ResponseTime.java
@@ -1,6 +1,7 @@
 package ak_ss.balancer.algorithms;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import ak_ss.balancer.Algorithm;
@@ -13,19 +14,64 @@ import ak_ss.balancer.Task;
  * @version 23.02.2016
  *
  */
-public class ResponseTime implements Algorithm {
+public class ResponseTime implements Algorithm, Runnable {
 
 	private Collection<Node> nodes;
 	private Node fastest;
+	private boolean lauf;
 
 	public ResponseTime(List nodes){
 		this.nodes = nodes;
+		new Thread(this).start();
 	}
-	
+
+	@Override
+	public void run(){
+		lauf = true;
+		while (lauf) {
+			calculateFastest();
+			try {
+				Thread.sleep(10000);
+			} catch (Exception e) {
+				e.printStackTrace();
+				lauf = false;
+			}
+		}
+	}
+
+	public void stop(){
+		lauf = false;
+	}
+
+	public void calculateFastest(){
+		Iterator<Node> i = nodes.iterator();
+		RuntimeSave rs = new RuntimeSave();
+		while (i.hasNext()) {
+			Node n = i.next();
+			Task test = new Task("1000000");
+			test.onFinish(() -> {
+				if(rs.setRuntime(test.getRunTime())){
+					fastest = n;
+				}
+			});
+			n.addTask(test);
+		}
+	}
+
 	@Override
 	public Node getNext() {
-		Task test = new Task("{\"options\":{}}");
-		return null;
+		return fastest;
 	}
-	
+
+	private  class RuntimeSave{
+		private long runtime = -1;
+		public boolean setRuntime(long runtime){
+			if(this.runtime < 0 || this.runtime > runtime){
+				this.runtime = runtime;
+				return true;
+			}
+			return false;
+		}
+		public long getRuntime(){ return runtime; }
+	}
 }
